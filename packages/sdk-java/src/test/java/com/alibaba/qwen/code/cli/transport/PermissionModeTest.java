24d11179d881022915f2fef908207419bb951ee3
乾离 <gengwei.gw@alibaba-inc.com>
2025-12-23 20:04:58 +0800

    modify junit version to 5 and add org developers

--- packages/sdk-java/src/test/java/com/alibaba/qwen/code/cli/transport/PermissionModeTest.java
+++ packages/sdk-java/src/test/java/com/alibaba/qwen/code/cli/transport/PermissionModeTest.java
@@ -1,16 +1,17 @@
 package com.alibaba.qwen.code.cli.transport;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PermissionModeTest {
 
     @Test
     public void shouldBeReturnQwenPermissionModeValue() {
-        Assert.assertEquals("default", PermissionMode.DEFAULT.getValue());
-        Assert.assertEquals("plan", PermissionMode.PLAN.getValue());
-        Assert.assertEquals("auto-edit", PermissionMode.AUTO_EDIT.getValue());
-        Assert.assertEquals("yolo", PermissionMode.YOLO.getValue());
+        assertEquals("default", PermissionMode.DEFAULT.getValue());
+        assertEquals("plan", PermissionMode.PLAN.getValue());
+        assertEquals("auto-edit", PermissionMode.AUTO_EDIT.getValue());
+        assertEquals("yolo", PermissionMode.YOLO.getValue());
     }
 
 }
